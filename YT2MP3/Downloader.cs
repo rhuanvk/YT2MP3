0c9d999128838f01e3ed0925a1b50af04d830f8b
Rhuan Klassmann <rhuan.klassmann@forbos.com.br>
2024-10-18 17:20:33 -0300

    Add project files.

---
+++ YT2MP3/Downloader.cs
@@ -0,0 +1,35 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace YT2MP3
+{
+    internal class Downloader
+    {
+        public async Task DownloadYoutubeVideo(string url)
+        {
+
+            var youtube = new YoutubeClient();
+            var video = await youtube.Videos.GetAsync(url);
+            
+            Console.WriteLine($"Convertendo o aúdio do vídeo: {video.Title}");
+
+            string tituloFormatado = string.Join("", video.Title.Split(Path.GetInvalidFileNameChars()));
+            string dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            var manifestoStream = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+            var audioStream = manifestoStream
+                .GetAudioOnlyStreams()
+                .Where(s => s.Container == Container.Mp4)
+                .GetWithHighestBitrate();
+
+            try
+            {
+                await youtube.Videos.Streams.DownloadAsync(audioStream, $"{dir}\\{tituloFormatado}.mp3");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ YT2MP3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace YT2MP3
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("##### Conversor de vídeos para .mp3 #####\n");
+            Console.WriteLine("Informe a URL do video a ser convertido:");
+            
+            string url = Console.ReadLine();
+
+            Console.WriteLine("Iniciando download...");
+           
+            Downloader downloader = new Downloader();
+            await downloader.DownloadYoutubeVideo(url);
+
+            Console.WriteLine("Download finalizado. Verifique sua área de trabalho.\n");
+            Console.WriteLine("Pressione qualquer tecla para finalizar.");
+            Console.WriteLine("#########################################");
+            Console.ReadKey();
+        }
+    }
+}
